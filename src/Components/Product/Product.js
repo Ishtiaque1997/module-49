1e9afdf3bd319d79ea5f8213d64f8b2a453cea3a
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-13 07:27:43 +0600

    first-commit

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css';
+const Product = (props) => {
+ // console.log(props)
+ const{img,name,price,seller,stock}=props.product;
+ return (
+  <div className='product'>
+   <div >
+    <img src={img} alt="" />
+   </div>
+   <div className='product-details'>
+    <h5>{name}</h5>
+    <p>By:{seller}</p>
+    <p>Price:{price}</p>
+    <p>Only {stock} available. Order soon</p>
+   </div>
+      
+  </div>
+ );
+};
+
+export default Product;<h1>This is product</h1>

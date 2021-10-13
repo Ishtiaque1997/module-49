f39b969bb3ed8d73a161d40f18f7f416817e35c4
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-13 07:43:45 +0600

    edit-done

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -10,13 +10,13 @@ const Product = (props) => {
    </div>
    <div className='product-details'>
     <h5>{name}</h5>
-    <div className='info'>
+   
      <p>By:{seller}</p>
     <p>Price:${price}</p>
     <p>Only {stock} available. Order soon</p>
     <button>Add to cart</button>
     </div>
-   </div>
+   
       
   </div>
  );

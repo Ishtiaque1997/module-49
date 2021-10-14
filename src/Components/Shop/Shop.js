2f2d17cdec555b0d0473eb9d56a94eabdb7ebc6a
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-14 07:07:01 +0600

    cart still going on

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -8,15 +8,7 @@ const Shop = () => {
  const[products,setProducts]=useState([]);
 
  const [cart,setCart]=useState([]);
- useEffect(()=>{
-  const savedCart=getStoredCart();
-  for (const key in savedCart ){
-    // console.log(key);
-    const addedProduct=products.find(product=>product.key===key);
-    console.log(key,addedProduct)
-  }
- },[products])
-
+ 
  const handleAddToCart=(product)=>{
   // console.log('btn clicked',product)
   const newCart=[...cart,product];
@@ -28,6 +20,19 @@ const Shop = () => {
   .then(res=>res.json())
   .then(data=>setProducts(data))
  },[])
+
+ useEffect(()=>{
+  const savedCart=getStoredCart();
+  const storedCart=[];
+  if(products.length){
+    for (const key in savedCart ){
+    // console.log(key);
+    const addedProduct=products.find(product=>product.key===key);
+    storedCart.push(addedProduct);
+  }
+  setCart(storedCart)
+  }
+ },[products])
  return (
   <div className='shop-container'>
    <div className='product-container'>
